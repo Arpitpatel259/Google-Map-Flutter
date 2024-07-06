e947536d81f27e01cba75bcc9989a423d80be92b
Arpit Vekariya <arpit.vekariya123@gmail,com>
2024-07-06 16:54:54 +0530

    Commit

--- lib/mapview.dart
+++ lib/mapview.dart
@@ -1,336 +1,262 @@
-import 'package:flutter/foundation.dart';
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:flutter_polyline_points/flutter_polyline_points.dart';
-import 'package:geocoding/geocoding.dart';
+import 'package:flutter/services.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'dart:math' show max, min;
+import 'package:geocoding/geocoding.dart';
 
-class MapView extends StatefulWidget {
-  const MapView({super.key});
-
+class MapScreen extends StatefulWidget {
   @override
-  _MapViewState createState() => _MapViewState();
+  _MapScreenState createState() => _MapScreenState();
 }
 
-class _MapViewState extends State<MapView> {
-  late GoogleMapController mapController;
+class _MapScreenState extends State<MapScreen> {
+  GoogleMapController? _controller;
+  TextEditingController _startPointController = TextEditingController();
+  TextEditingController _destinationController = TextEditingController();
+  List<LatLng> _polylineCoordinates = [];
+  Set<Polyline> _polylines = {};
+  Set<Marker> _markers = {};
+  String _distance = "0.0";
   final CameraPosition _initialLocation =
-      const CameraPosition(target: LatLng(0.0, 0.0));
-  Position? _currentPosition;
-  String _currentAddress = '';
+      const CameraPosition(target: LatLng(37.77483, -122.41942), zoom: 12);
 
-  final startAddressController = TextEditingController();
-  final destinationAddressController = TextEditingController();
+  String _darkMapStyle = '';
+  String _lightMapStyle = '';
+  bool _isDarkTheme = false;
 
-  final startAddressFocusNode = FocusNode();
-  final destinationAddressFocusNode = FocusNode();
-
-  String _startAddress = '';
-  String _destinationAddress = '';
-
-  Set<Marker> markers = {};
-  late PolylinePoints polylinePoints;
-  Map<PolylineId, Polyline> polylines = {};
-  List<LatLng> polylineCoordinates = [];
+  LatLng? _currentLocation;
 
   @override
   void initState() {
     super.initState();
-    polylinePoints = PolylinePoints();
-    _getCurrentLocation();
+    _loadMapStyles();
   }
 
-  @override
-  void dispose() {
-    startAddressFocusNode.dispose();
-    destinationAddressFocusNode.dispose();
-    startAddressController.dispose();
-    destinationAddressController.dispose();
-    super.dispose();
-  }
-
-  // Method for retrieving the current location
-  void _getCurrentLocation() async {
-    bool serviceEnabled;
-    LocationPermission permission;
-
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnabled) {
-      throw 'Location services are disabled.';
-    }
-
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        throw 'Location permissions are denied';
-      }
-    }
-
-    if (permission == LocationPermission.deniedForever) {
-      throw 'Location permissions are permanently denied, we cannot request permissions.';
-    }
-
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.high);
-
-    setState(() {
-      _currentPosition = position;
-      mapController.animateCamera(
-        CameraUpdate.newCameraPosition(
-          CameraPosition(
-            target: LatLng(position.latitude, position.longitude),
-            zoom: 18.0,
-          ),
-        ),
-      );
-    });
-
-    await _getAddress();
-  }
-
-  // Method for retrieving the address
-  Future<void> _getAddress() async {
-    try {
-      List<Placemark> p = await placemarkFromCoordinates(
-        _currentPosition!.latitude,
-        _currentPosition!.longitude,
-      );
-
-      Placemark place = p[0];
-
-      setState(() {
-        _currentAddress =
-            "${place.name}, ${place.locality}, ${place.postalCode}, ${place.country}";
-        startAddressController.text = _currentAddress;
-        _startAddress = _currentAddress;
-      });
-    } catch (e) {
-      if (kDebugMode) {
-        print(e);
-      }
-    }
-  }
-
-  // Method for calculating the distance between two places
-  Future<bool> _calculateDistance() async {
-    try {
-      // Fetching start and destination locations
-      List<Location> startPlacemark = await locationFromAddress(_startAddress);
-      List<Location> destinationPlacemark =
-          await locationFromAddress(_destinationAddress);
-
-      double startLatitude = _startAddress == _currentAddress
-          ? _currentPosition!.latitude
-          : startPlacemark[0].latitude;
-      double startLongitude = _startAddress == _currentAddress
-          ? _currentPosition!.longitude
-          : startPlacemark[0].longitude;
-
-      double destinationLatitude = destinationPlacemark[0].latitude;
-      double destinationLongitude = destinationPlacemark[0].longitude;
-
-      markers.clear();
-
-      // Creating markers for start and destination
-      String startCoordinatesString = '($startLatitude, $startLongitude)';
-      String destinationCoordinatesString =
-          '($destinationLatitude, $destinationLongitude)';
-
-      Marker startMarker = Marker(
-        markerId: MarkerId(startCoordinatesString),
-        position: LatLng(startLatitude, startLongitude),
-        infoWindow: InfoWindow(
-          title: 'Start $startCoordinatesString',
-          snippet: _startAddress,
-        ),
-        icon: BitmapDescriptor.defaultMarker,
-      );
-
-      Marker destinationMarker = Marker(
-        markerId: MarkerId(destinationCoordinatesString),
-        position: LatLng(destinationLatitude, destinationLongitude),
-        infoWindow: InfoWindow(
-          title: 'Destination $destinationCoordinatesString',
-          snippet: _destinationAddress,
-        ),
-        icon: BitmapDescriptor.defaultMarker,
-      );
-
-      // Adding markers to the map
-      setState(() {
-        markers.add(startMarker);
-        markers.add(destinationMarker);
-      });
-
-      // Adjusting camera to fit both markers
-      double miny = min(startLatitude, destinationLatitude);
-      double minx = min(startLongitude, destinationLongitude);
-      double maxy = max(startLatitude, destinationLatitude);
-      double maxx = max(startLongitude, destinationLongitude);
-
-      mapController.animateCamera(
-        CameraUpdate.newLatLngBounds(
-          LatLngBounds(
-            northeast: LatLng(maxy, maxx),
-            southwest: LatLng(miny, minx),
-          ),
-          100.0,
-        ),
-      );
-
-      // Creating polylines and calculating distance
-      await _createPolylines(startLatitude, startLongitude, destinationLatitude,
-          destinationLongitude);
-
-      return true;
-    } catch (e) {
-      if (kDebugMode) {
-        print(e.toString());
-      }
-      return false;
-    }
-  }
-
-  // Create the polylines for showing the route between two places
-  Future<void> _createPolylines(double startLatitude, double startLongitude,
-      double destinationLatitude, double destinationLongitude) async {
-    try {
-      PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
-        'AIzaSyCl4Yjbq5GiZYPBS3TfoqV7YOmHCEjQCIU', // Replace with your Google Maps API Key
-        PointLatLng(startLatitude, startLongitude),
-        PointLatLng(destinationLatitude, destinationLongitude),
-        travelMode: TravelMode.driving,
-      );
-
-      if (result.points.isNotEmpty) {
-        List<LatLng> newPolylineCoordinates = result.points
-            .map((point) => LatLng(point.latitude, point.longitude))
-            .toList();
-
-        PolylineId id = const PolylineId('poly');
-        Polyline polyline = Polyline(
-          polylineId: id,
-          color: Colors.blue,
-          points: newPolylineCoordinates,
-          width: 3,
-          jointType: JointType.round,
-          endCap: Cap.roundCap,
-          startCap: Cap.roundCap,
-        );
-
-        setState(() {
-          polylines[id] = polyline;
-        });
-      } else {
-        if (kDebugMode) {
-          print('No polyline points found');
-        }
-      }
-    } catch (e) {
-      if (kDebugMode) {
-        print('Error: $e');
-      }
-    }
+  Future<void> _loadMapStyles() async {
+    _darkMapStyle = await rootBundle.loadString('assets/dark_map_style.json');
+    _lightMapStyle = await rootBundle.loadString('assets/light_map_style.json');
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          GoogleMap(
-            initialCameraPosition: _initialLocation,
-            myLocationEnabled: true,
-            myLocationButtonEnabled: false,
-            mapType: MapType.normal,
-            zoomGesturesEnabled: true,
-            zoomControlsEnabled: false,
-            markers: markers,
-            polylines: Set<Polyline>.of(polylines.values),
-            onMapCreated: (GoogleMapController controller) {
-              mapController = controller;
-            },
-          ),
-          Positioned(
-            top: 50.0,
-            right: 15.0,
-            left: 15.0,
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10.0),
-                boxShadow: const [
-                  BoxShadow(
-                    color: Colors.grey,
-                    offset: Offset(0.0, 1.0), // (x, y)
-                    blurRadius: 6.0,
-                  ),
-                ],
-              ),
-              child: Column(
-                children: <Widget>[
-                  TextField(
-                    controller: startAddressController,
-                    focusNode: startAddressFocusNode,
-                    decoration: InputDecoration(
-                      hintText: 'Start Address',
-                      border: InputBorder.none,
-                      contentPadding: const EdgeInsets.only(left: 15.0, top: 15.0),
-                      suffixIcon: IconButton(
-                        icon: const Icon(Icons.cancel),
-                        onPressed: () => startAddressController.clear(),
-                      ),
-                    ),
-                    onChanged: (value) {
-                      setState(() {
-                        _startAddress = value;
-                      });
-                    },
-                  ),
-                  const Divider(height: 1.0, color: Colors.grey),
-                  TextField(
-                    controller: destinationAddressController,
-                    focusNode: destinationAddressFocusNode,
-                    decoration: InputDecoration(
-                      hintText: 'Destination Address',
-                      border: InputBorder.none,
-                      contentPadding: const EdgeInsets.only(left: 15.0, top: 15.0),
-                      suffixIcon: IconButton(
-                        icon: const Icon(Icons.cancel),
-                        onPressed: () => destinationAddressController.clear(),
-                      ),
-                    ),
-                    onChanged: (value) {
-                      setState(() {
-                        _destinationAddress = value;
-                      });
-                    },
-                  ),
-                  const SizedBox(height: 10.0),
-                  ElevatedButton(
-                    onPressed:
-                        (_startAddress != '' && _destinationAddress != '')
-                            ? () async {
-                                await _calculateDistance();
-                              }
-                            : null,
-                    child: const Text('Show Route'),
-                  ),
-                  const SizedBox(height: 10.0),
-                ],
-              ),
-            ),
+      appBar: AppBar(
+        title: const Text('Google Maps'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.dark_mode_outlined),
+            onPressed: _toggleMapTheme,
           ),
         ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          _getCurrentLocation();
-        },
-        child: const Icon(Icons.location_searching),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _startPointController,
+                    decoration: const InputDecoration(
+                      labelText: 'Starting Point',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 10),
+                Expanded(
+                  child: TextField(
+                    controller: _destinationController,
+                    decoration: const InputDecoration(
+                      labelText: 'Destination',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          ElevatedButton(
+            onPressed: _calculateDistanceFromInput,
+            child: const Text('Calculate Distance'),
+          ),
+          Expanded(
+            child: GoogleMap(
+              initialCameraPosition: _initialLocation,
+              myLocationEnabled: true,
+              myLocationButtonEnabled: true,
+              mapType: MapType.normal,
+              zoomGesturesEnabled: true,
+              zoomControlsEnabled: false,
+              markers: _markers,
+              polylines: _polylines,
+              onMapCreated: (GoogleMapController controller) {
+                _controller = controller;
+                _setMapStyle();
+              },
+              onTap: _handleMapTap,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text("Distance: $_distance km"),
+          ),
+          if (_currentLocation != null)
+            ElevatedButton.icon(
+              onPressed: _goToCurrentLocation,
+              icon: Icon(Icons.location_on),
+              label: Text('Show Current Location'),
+            ),
+        ],
       ),
     );
+  }
+
+  void _handleMapTap(LatLng tappedPosition) {
+    _markers.clear(); // Clear all markers
+    _polylineCoordinates.clear(); // Clear all polyline coordinates
+
+    _addMarker(tappedPosition);
+  }
+
+  void _addMarker(LatLng position) {
+    setState(() {
+      _markers.add(
+        Marker(
+          markerId: MarkerId(position.toString()),
+          position: position,
+        ),
+      );
+
+      _polylineCoordinates.add(position);
+
+      if (_markers.length == 2) {
+        _calculateDistanceFromInput();
+      }
+    });
+  }
+
+  void _drawPolyline() {
+    setState(() {
+      _polylines.add(
+        Polyline(
+          polylineId: PolylineId('polyline'),
+          visible: true,
+          points: _polylineCoordinates,
+          color: Colors.blue,
+        ),
+      );
+    });
+  }
+
+  void _calculateDistanceFromInput() async {
+    _polylines.clear(); // Clear any existing polylines
+
+    String startPoint = _startPointController.text;
+    String destination = _destinationController.text;
+
+    if (startPoint.isEmpty || destination.isEmpty) {
+      _showSnackbar('Please enter both starting point and destination.');
+      return;
+    }
+
+    var startCoordinates = await _getCoordinates(startPoint);
+    var destCoordinates = await _getCoordinates(destination);
+
+    if (startCoordinates != null && destCoordinates != null) {
+      _polylineCoordinates = [startCoordinates, destCoordinates];
+      _markers = {
+        Marker(
+          markerId: MarkerId(startCoordinates.toString()),
+          position: startCoordinates,
+        ),
+        Marker(
+          markerId: MarkerId(destCoordinates.toString()),
+          position: destCoordinates,
+        ),
+      };
+      _drawPolyline();
+      _calculateDistance(startCoordinates, destCoordinates);
+    } else {
+      _showSnackbar('Could not determine coordinates for input points.');
+    }
+  }
+
+  Future<LatLng?> _getCoordinates(String address) async {
+    try {
+      List<Location> locations = await locationFromAddress(address);
+      if (locations != null && locations.isNotEmpty) {
+        Location location = locations.first;
+        return LatLng(location.latitude, location.longitude);
+      } else {
+        return null;
+      }
+    } catch (e) {
+      print('Error retrieving coordinates: $e');
+      return null;
+    }
+  }
+
+  void _calculateDistance(LatLng start, LatLng destination) {
+    double distance = Geolocator.distanceBetween(
+          start.latitude,
+          start.longitude,
+          destination.latitude,
+          destination.longitude,
+        ) /
+        1000; // Convert to kilometers
+    setState(() {
+      _distance = distance.toStringAsFixed(2);
+    });
+  }
+
+  void _toggleMapTheme() {
+    setState(() {
+      _isDarkTheme = !_isDarkTheme;
+      _setMapStyle();
+    });
+  }
+
+  void _setMapStyle() {
+    if (_controller != null) {
+      _controller!.setMapStyle(_isDarkTheme ? _darkMapStyle : _lightMapStyle);
+    }
+  }
+
+  void _showSnackbar(String message) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text(message),
+    ));
+  }
+
+  void _goToCurrentLocation() async {
+    try {
+      Position position = await Geolocator.getCurrentPosition();
+      LatLng latLng = LatLng(position.latitude, position.longitude);
+
+      setState(() {
+        _currentLocation = latLng;
+      });
+
+      _controller?.animateCamera(
+        CameraUpdate.newCameraPosition(
+          CameraPosition(
+            target: latLng,
+            zoom: 15,
+          ),
+        ),
+      );
+    } catch (e) {
+      _showSnackbar('Error getting current location: $e');
+    }
+  }
+
+  @override
+  void dispose() {
+    _startPointController.dispose();
+    _destinationController.dispose();
+    super.dispose();
   }
 }
